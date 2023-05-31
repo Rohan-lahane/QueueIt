7a747b717ab7caba7cb8b085ed15d8cde3d5af5c
Rohan-lahane <rohanlahane30@gmail.com>
2023-05-31 09:54:30 +0530

    testing spotify login

--- Client/src/Testing/SpotifyLogin.jsx
+++ Client/src/Testing/SpotifyLogin.jsx
@@ -53,7 +53,7 @@ const useAuth = (code) => {
 
 const Login = () => {
   const AUTH_URL =
-    "https://accounts.spotify.com/authorize?client_id=58f6327adb8d406b86b39a7c330cd125&response_type=code&redirect_uri=https://queue-it.netlify.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+    "https://accounts.spotify.com/authorize?client_id=58f6327adb8d406b86b39a7c330cd125&response_type=code&redirect_uri=https://queue-it.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
   return (
     <div className="spotifyLogin">
